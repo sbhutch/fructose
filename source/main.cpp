7eadac839ab1254439d7f39ec9880cce93cf96f9
sbhutch <sb.hutch@icloud.com>
2022-08-01 23:59:55 +0930

    main: line vs rect intersect demo

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,6 @@
-#include <algorithm>
 #include <SDL.h>
+#include <algorithm>
+#include <array>
 
 bool intersects(SDL_FPoint o, SDL_FPoint d, SDL_FRect r)
 {
@@ -13,6 +14,35 @@ bool intersects(SDL_FPoint o, SDL_FPoint d, SDL_FRect r)
     float tmax = std::min(std::max(tx1, tx2), std::max(ty1, ty2));
 
     return tmin >= 0 && tmin <= 1 && tmax >= tmin;
+}
+
+void draw_scene(SDL_Renderer* renderer)
+{
+    int x, y;
+    SDL_GetMouseState(&x, &y);
+
+    SDL_FPoint a { 240, 135 };
+    SDL_FPoint b { static_cast<float>(x), static_cast<float>(y) };
+    SDL_FPoint d { b.x - a.x, b.y - a.y };
+
+    std::array rs = {
+        SDL_FRect { 40.0f, 40.0f, 400.0f, 20.0f },
+        SDL_FRect { 100.0f, 80.0f, 20.0f, 120.0f },
+        SDL_FRect { 360.0f, 80.0f, 20.0f, 120.0f },
+        SDL_FRect { 220.0f, 180.0f, 40.0f, 40.0f },
+    };
+
+    for (auto r : rs) {
+        if (intersects(a, d, r)) {
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        } else {
+            SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+        }
+        SDL_RenderDrawRectF(renderer, &r);
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    SDL_RenderDrawLineF(renderer, a.x, a.y, b.x, b.y);
 }
 
 int main(int, char**)
@@ -46,6 +76,7 @@ int main(int, char**)
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
+        draw_scene(renderer);
         SDL_RenderPresent(renderer);
     }
 
